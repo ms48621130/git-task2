b073ff85f093d980e9361e113e14674f392698d0
EC2 Default User <ec2-user@ip-172-31-35-2.ap-northeast-1.compute.internal>
2022-10-16 19:07:28 +0000

    [Add] task2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,10 @@ class BooksController < ApplicationController
   end
     
   def destroy
- end
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
   
   private
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-get '/' => "homes#top"
-get 'books' => 'books#index'
-post 'books' => 'books#create'
-
-
+resources :books
 end
